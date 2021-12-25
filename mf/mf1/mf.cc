b99ba661c9e80d9228f78332276d10b37f326f20
Yao-Yin <InYuo1997@gmail.com>
2021-12-25 10:27:22 +0800

    MF1 baseline

--- mf/mf1/mf.cc
+++ mf/mf1/mf.cc
@@ -1,3 +1,6 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
 /*
 This is the function you need to implement. Quick reference:
 - input rows: 0 <= y < ny
@@ -7,5 +10,36 @@ This is the function you need to implement. Quick reference:
   max(x-hx, 0) <= a < min(x+hx+1, nx), max(y-hy, 0) <= b < min(y+hy+1, ny)
   in out[x + y*nx].
 */
+double get_median(vector<double> & v, size_t tot) {
+  if (tot & 1) {
+    nth_element(v.begin(), v.begin() + tot/2, v.begin() + tot);
+    return v[tot/2];
+  }
+  nth_element(v.begin(), v.begin() + tot/2, v.begin() + tot);
+  double p1 = v[tot/2];
+  nth_element(v.begin(), v.begin() + tot/2 - 1, v.begin() + tot);
+  double p2 = v[tot/2 - 1];
+  return (p1 + p2)/2;
+}
+
 void mf(int ny, int nx, int hy, int hx, const float *in, float *out) {
+  int lb{0}, rb{0}, ub{0}, db{0}, idx{0};
+  double median = 0.0;
+  vector<double> v((2*hx+1)*(2*hy+1), 0.0);
+  for (int y = 0; y < ny; y ++) {
+    for (int x = 0; x < nx; x ++) {
+      ub = std::max(y-hy, 0);
+      db = std::min(y+hy+1, ny);
+      lb = std::max(x-hx, 0);
+      rb = std::min(x+hx+1, nx);
+      idx = 0;
+      for (int ry = ub; ry < db; ry ++) {
+        for (int rx = lb; rx < rb; rx ++) {
+          v[idx ++] = *(in + ry*nx + rx);
+        }
+      }
+      median = get_median(v, idx);
+      *(out + y*nx + x) = median; 
+    }
+  }
 }
